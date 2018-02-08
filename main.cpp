6548533afd049f000b007e00200e9cea317171ee
aralmheiri <aralmheiri@mail.csuchico.edu>
2018-02-08 11:59:02 -0800

    upper case letters

--- main.cpp
+++ main.cpp
@@ -1,24 +1,37 @@
-//Author:
+//Author: aralmheiri
 #include<iostream>
 
 using namespace std;
 
 int main()
 {
-
+char A;
   cout<<"What character do you want to know about?\n";
+ cin>>A; 
+ 
+ 
+ if (A>='A' && A<='Z')
+ {
 
-  //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+ 
+ //when user's entry is between A-Z...
+  cout<<A<<" is an upper case letter!\n";
+}
 
-  //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
 
-  //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
-  
+else if (A>='a' && A<='z')
+{
+  //when user's entr is between a-z...
+  cout<<A<<" is a lower case letter!\n";
+}
+
+else {
+
+//in all other cases...
+  cout<<A<<"?! Pssh. What are you talking about?\n";
+  }
   //no matter what they enter...
-  cout<<"The ASCII value is: ";
+  cout<<"The ASCII value is: "<<(int)A<<endl;
 
   return 0;
 }
